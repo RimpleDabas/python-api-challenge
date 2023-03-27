717a86d1d1cf20a270c008daf7c4074eee01417c
RimpleDabas <125090606+RimpleDabas@users.noreply.github.com>
2023-03-27 16:29:31 -0400

    Weather stats and plots generated

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a35817266ac24b1cd47698af0f509b90"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a589d30831524090b28f6922e9653c8b"
